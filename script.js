8ffe061174fcbc07ec1431a3ee341f4ba2de2875
NeroCoreNull <167572182+NeroCoreNull@users.noreply.github.com>
2024-04-26 09:41:20 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('footer').addEventListener('click', function() {
+    var hiddenLink = document.getElementById('hidden-link');
+    hiddenLink.style.opacity = '1'; // This makes the link visible
+});
